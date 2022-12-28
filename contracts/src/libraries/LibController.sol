616dffbe282ee83fc2ef44b6dadf7cd3426978c9
smallbraingames <smallbraindev@gmail.com>
2022-12-27 21:59:30 -0800

    feat(contracts): set piece type system
    - lib logic for checking if can control (eventually make granular)
    - basic test

--- contracts/src/libraries/LibController.sol
+++ contracts/src/libraries/LibController.sol
@@ -1,32 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import { ControllerComponent } from "components/ControllerComponent.sol";
-import { NotController, AlreadyHasController } from "common/Errors.sol";
+import { OwnerComponent } from "components/OwnerComponent.sol";
+import { LibOwner } from "libraries/LibOwner.sol";
+import { NotController, AlreadyHasController, CannotControl } from "common/Errors.sol";
 
 library LibController {
   /// @notice Checks if an entity has no controllers
   function checkNoControllers(ControllerComponent controllerComponent, uint256 entity) internal view {
-    if (!controllerComponent.has(entity)) return;
-    uint256[] memory paddedControllers = controllerComponent.getValue(entity);
-    if (paddedControllers.length > 0) {
+    if (getNumControllers(controllerComponent, entity) > 0) {
       revert AlreadyHasController();
     }
   }
 
-  /// @notice Checks if an address is the owner of an entity
+  /// @notice Returns number of controllers of an entity
+  function getNumControllers(ControllerComponent controllerComponent, uint256 entity) internal view returns (uint256) {
+    if (!controllerComponent.has(entity)) return 0;
+    uint256[] memory paddedControllers = controllerComponent.getValue(entity);
+    return paddedControllers.length;
+  }
+
+  /// @notice Returns whether an address is the controller of an entity
+  function isController(
+    ControllerComponent controllerComponent,
+    uint256 entity,
+    address checkAddress
+  ) internal view returns (bool) {
+    if (!controllerComponent.has(entity)) {
+      return false;
+    }
+    uint256[] memory paddedControllers = controllerComponent.getValue(entity);
+    for (uint256 i = 0; i < paddedControllers.length; i++) {
+      if (unpadAddress(paddedControllers[i]) == checkAddress) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /// @notice Checks if an address is the controller of an entity
   function checkIsController(
     ControllerComponent controllerComponent,
     uint256 entity,
     address checkAddress
   ) internal view {
-    if (!controllerComponent.has(entity)) revert NotController();
-    uint256[] memory paddedControllers = controllerComponent.getValue(entity);
-    for (uint256 i = 0; i < paddedControllers.length; i++) {
-      if (unpadAddress(paddedControllers[i]) == checkAddress) {
-        return;
-      }
+    if (!isController(controllerComponent, entity, checkAddress)) {
+      revert NotController();
     }
-    revert NotController();
+  }
+
+  /// @notice Checks whether an address can control an entity
+  /// Checks whether an address is a controller–unless there are no controllers,
+  /// in which case, check whether the address is an owner
+  function checkCanControl(
+    ControllerComponent controllerComponent,
+    OwnerComponent ownerComponent,
+    uint256 entity,
+    address checkAddress
+  ) internal view {
+    if (isController(controllerComponent, entity, checkAddress)) {
+      return;
+    }
+    if (getNumControllers(controllerComponent, entity) == 0 && LibOwner.isOwner(ownerComponent, entity, checkAddress)) {
+      return;
+    }
+    revert CannotControl();
   }
 
   /// @notice Remove a controller
