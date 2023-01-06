c1c297f2495aac1a74aaa66e3b5bea5f9fa7fc63
smallbraingames <smallbraindev@gmail.com>
2023-01-05 18:15:08 -0800

    fix(contracts): wrong component id for BRInGameComponent

--- contracts/src/components/BRInGameComponent.sol
+++ contracts/src/components/BRInGameComponent.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 import "std-contracts/components/Uint256Component.sol";
 
-uint256 constant ID = uint256(keccak256("component.BRInGameComponent"));
+uint256 constant ID = uint256(keccak256("component.BRInGame"));
 
 contract BRInGameComponent is Uint256Component {
   constructor(address world) Uint256Component(world, ID) {}
