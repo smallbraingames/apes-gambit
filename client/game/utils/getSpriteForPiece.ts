d5c112ef20dc596e09c29606e988b4f45d01b66a
smallbraingames <smallbraindev@gmail.com>
2023-01-16 16:35:31 -0800

    feat(client): display enemy pieces as purple sprites

--- client/game/utils/getSpriteForPiece.ts
+++ client/game/utils/getSpriteForPiece.ts
@@ -5,14 +5,33 @@ import { Sprites } from "../constants";
 
 const getSpriteForPiece = (
   pieceEntity: EntityIndex,
-  network: Network
+  network: Network,
+  isEnemy: boolean = false
 ): Sprites => {
   const pieceType: PieceType | undefined = getComponentValue(
     network.components.PieceType,
     pieceEntity
   )?.value;
   if (pieceType === undefined) throw Error("Piece type not found");
-  // Don't worry about the owner / active piece logic rn
+
+  if (isEnemy) {
+    console.log("is ENEMY!!!!!!");
+    switch (pieceType) {
+      case PieceType.PAWN:
+        return Sprites.EnemyPawn;
+      case PieceType.BISHOP:
+        return Sprites.EnemyBishop;
+      case PieceType.KNIGHT:
+        return Sprites.EnemyKnight;
+      case PieceType.ROOK:
+        return Sprites.EnemyRook;
+      case PieceType.QUEEN:
+        return Sprites.EnemyQueen;
+      case PieceType.KING:
+        return Sprites.EnemyKing;
+    }
+  }
+
   switch (pieceType) {
     case PieceType.PAWN:
       return Sprites.MainPawn;
@@ -25,7 +44,7 @@ const getSpriteForPiece = (
     case PieceType.QUEEN:
       return Sprites.MainQueen;
     case PieceType.KING:
-      return Sprites.MainQueen;
+      return Sprites.MainKing;
   }
 };
 
