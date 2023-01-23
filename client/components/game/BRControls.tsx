2197aeacbee1d8acdc475705c34a0861a6328b0d
pillhead <hellopillhead@protonmail.com>
2023-01-23 12:58:53 +1100

    add in + comment out components. update structure per figma

--- client/components/game/BRControls.tsx
+++ client/components/game/BRControls.tsx
@@ -9,6 +9,8 @@ import JoinGame from "./JoinGame";
 import Points from "./Points";
 import SpawnPiece from "./SpawnPiece";
 import UpgradePiece from "./UpgradePiece";
+import SummaryCard from "./SummaryCard";
+import PlayerCount from "./PlayerCount";
 
 const BRControls = () => {
   const game = useContext(GameContext);
@@ -21,15 +23,22 @@ const BRControls = () => {
     <>
       <div className={CONTROLLER_COMPONENT_CLASS_NAME}>
         {game.game && (
-          <div>
-            {/* <JoinGame game={game.game} />
-            <SpawnPiece /> */}
-            <div className="mb-2">
+          <div className="block">
+            <div className="w-full flex justify-between mb-2">
               <Points />
+              <PlayerCount />
             </div>
             <div>
-              <UpgradePiece />
+              <JoinGame game={game.game} />
+              <SpawnPiece />
             </div>
+            Displays the UpgradePiece window
+            {/* <div>
+              <UpgradePiece />
+            </div> */}
+            {/* <div>
+              <SummaryCard />
+            </div> */}
           </div>
         )}
       </div>
