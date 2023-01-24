18dbfd38185f6a6b800a27d4e997d876f07f0ba2
pillhead <hellopillhead@protonmail.com>
2023-01-24 14:03:04 +1100

    feat(client): old Timer component code migrated to new GameClock component

---
+++ client/components/game/GameClock.tsx
@@ -0,0 +1,13 @@
+const GameClock = () => {
+  return (
+    <div className="container-gamestats">
+      <div className="h-10 w-10">[img]</div>
+      <div className="ml-2">
+        {/* TODO: Add function to countdown the timer */}
+        <div className="bignumber flex items-center">04:81</div>
+      </div>
+    </div>
+  );
+};
+
+export default GameClock;
