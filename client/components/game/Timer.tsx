0a6dc910f285114ba169462df9208f91978958a9
pillhead <hellopillhead@protonmail.com>
2023-01-24 14:02:14 +1100

    refactor(client): component is now player-specific action Timer

--- client/components/game/Timer.tsx
+++ client/components/game/Timer.tsx
@@ -1,10 +1,11 @@
 const Timer = () => {
+  {
+    /* TODO: Add function to control the timer after each action, conditionally style bg-color based on time  */
+  }
   return (
-    <div className="container-gamestats">
-      <div className="h-10 w-10">[img]</div>
-      <div className="ml-2">
-        {/* TODO: Add function to countdown the timer */}
-        <div className="bignumber flex items-center">04:81</div>
+    <div className="bg-green-600 flex p-1 rounded-full items-center w-48 h-16 my-1 border border-green-900 opacity-95">
+      <div className="w-full h-full rounded-full flex items-center justify-center bg-green-400">
+        <div className="bignumber flex items-center">3</div>
       </div>
     </div>
   );
