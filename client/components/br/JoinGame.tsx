ba2d7b2c3513d9af1b40918f486c6141e36d80b4
smallbraingames <smallbraindev@gmail.com>
2023-01-27 00:24:47 -0800

    fix(client): use active piece component in joingame

--- client/components/br/JoinGame.tsx
+++ client/components/br/JoinGame.tsx
@@ -1,22 +1,19 @@
 import { useContext, useEffect } from "react";
 
 import { Game } from "../../game/types";
+import { GameContext } from "../../context/GameContext";
 import { NetworkContext } from "../../context/NetworkContext";
-import { disableClickthroughs } from "../../utils/disableControllers";
 import getBurnerWallet from "../../network/wallet/getBurnerWallet";
 import getOwnedPieceEntityIndex from "../../game/utils/getOwnedPieceEntityIndex";
 import joinGame from "../../game/utils/joinGame";
 
 const JoinGame = (props: { game: Game }) => {
   const network = useContext(NetworkContext);
+  const game = useContext(GameContext);
 
   const handleJoinGame = () => {
-    const pieceEntity = getOwnedPieceEntityIndex(
-      getBurnerWallet().address,
-      network.network!.components.Owner,
-      network.network!.world
-    );
-    joinGame(pieceEntity, props.game.gameEntity!, network.network!);
+    if (game.activePiece)
+      joinGame(game.activePiece, props.game.gameEntity!, network.network!);
   };
 
   return (
