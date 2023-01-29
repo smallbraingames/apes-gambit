a13da7ecad1e82cd7a355bb11340b3b89625ddfc
smallbraingames <smallbraindev@gmail.com>
2023-01-28 18:29:57 -0800

    feat(client): loading fun

--- client/components/Title.tsx
+++ client/components/Title.tsx
@@ -37,7 +37,7 @@ const Title = () => {
 
   return (
     <div className={CONTROLLER_COMPONENT_CLASS_NAME}>
-      <div className="bg-stone-900 h-full w-full fixed flex items-start justify-center">
+      <div className="bg-green-100 h-full w-full fixed flex items-start justify-center lg:pt-24">
         <div className="flex flex-col justify-center rounded-xl w-full max-w-2xl p-8">
           <div className="flex flex-wrap justify-center">
             <Image
@@ -55,14 +55,40 @@ const Title = () => {
             />
           </div> */}
           </div>
-          <div className="flex flex-col items-center">
-            <button
-              className="btn btn-primary"
-              onClick={() => setLoadingState(LoadingState.IN_GAME)}
-            >
-              Play
-            </button>
-            <button className="btn btn-secondary">Help</button>
+          <div className="flex flex-col items-center mt-8">
+            <div className="flex items-center gap-2">
+              {loadingState === LoadingState.GAME_LOADED ? (
+                <button
+                  className="w-48 h-16 text-center bg-green-700 text-stone-100 rounded-lg border-b-4 border-r-2 border-green-900 text-xl active:border-b-0 active:border-r-0 active:translate-y-0.5"
+                  onClick={() => setLoadingState(LoadingState.IN_GAME)}
+                >
+                  PLAY
+                </button>
+              ) : (
+                <div className="w-48 h-16 text-center flex justify-center items-center text-stone-100 bg-stone-800 opacity-50 rounded-lg border-stone-900">
+                  {" "}
+                  Loading...{" "}
+                </div>
+              )}
+              <div
+                className={
+                  loadingState === LoadingState.GAME_LOADING
+                    ? "animate-spin"
+                    : "animate-bounce"
+                }
+              >
+                <Image
+                  src={
+                    loadingState === LoadingState.GAME_LOADING
+                      ? "/assets/sprites/main/attack/pawn.svg"
+                      : "/assets/sprites/main/attack/queen.svg"
+                  }
+                  width="80"
+                  height="80"
+                  alt="Angry"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
