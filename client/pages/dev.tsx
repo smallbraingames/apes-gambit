20b07c9cd4387bb001c146e896850b4bc50f818a
smallbraingames <smallbraindev@gmail.com>
2023-02-02 00:01:02 -0800

    feat(client): end game button on dev panel

--- client/pages/dev.tsx
+++ client/pages/dev.tsx
@@ -68,6 +68,14 @@ export default function Dev() {
                     >
                       Start Game
                     </button>
+                    <button
+                      onClick={() => {
+                        network.network?.api.br.endBRGame(entity as EntityID);
+                      }}
+                      className="ml-2 mt-2 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                      End Game
+                    </button>
                   </p>
                 );
               })}
