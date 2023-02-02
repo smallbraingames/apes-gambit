b0dbf84d5e967f463b2d65430ebc03bc54ac3651
smallbraingames <smallbraindev@gmail.com>
2023-02-01 19:34:50 -0800

    refactor(client): run spawning and joining logic in loader
    - when game is loaded, player is ready to go

--- client/components/lobby/Lobby.tsx
+++ client/components/lobby/Lobby.tsx
@@ -2,11 +2,8 @@ import {
   CONTROLLER_COMPONENT_CLASS_NAME,
   disableClickthroughs,
 } from "../../utils/disableControllers";
-import { LoadingContext, LoadingState } from "../../context/LoadingContext";
 
-import AddPlayerToMainGame from "./AddPlayerToMainGame";
 import CenterActivePieceOnLoad from "../CenterActivePieceOnLoad";
-import SpawnPieceIfNecessary from "./SpawnPieceIfNecessary";
 import { useEffect } from "react";
 
 const Lobby = () => {
@@ -18,8 +15,6 @@ const Lobby = () => {
     <>
       <div className={CONTROLLER_COMPONENT_CLASS_NAME}>
         <div className="absolute p-6">
-          <SpawnPieceIfNecessary />
-          <AddPlayerToMainGame />
           <CenterActivePieceOnLoad />
         </div>
       </div>
---
+++ client/game/utils/setup/spawnPiece.ts
@@ -0,0 +1,23 @@
+import { EntityIndex, getComponentValue } from "@latticexyz/recs";
+
+import { Game } from "../../types";
+import { Network } from "../../../network/types";
+
+const spawnPiece = async (network: Network, game: Game) => {
+  const { godEntityIndex, provider } = network;
+  const {
+    components: { ActivePiece },
+  } = game;
+  const activePiece = getComponentValue(ActivePiece, godEntityIndex);
+  if (activePiece) {
+    console.warn(
+      `Not spawning piece since active piece with entity index ${activePiece.value}`
+    );
+    return activePiece.value as EntityIndex;
+  }
+  console.log("Spawning new piece");
+  const tx = await network.api.spawnPiece();
+  await provider.waitForTransaction(tx.hash);
+};
+
+export default spawnPiece;
