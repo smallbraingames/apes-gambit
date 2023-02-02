21c9642e6516a0e4b833a2494f8be54a73ad0df6
smallbraingames <smallbraindev@gmail.com>
2023-02-02 00:00:44 -0800

    feat(contracts:br): dummy game end system - no checks are done, purely for dev

---
+++ contracts/src/systems/BREndGameSystem.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import { System, IWorld } from "solecs/System.sol";
+import { getAddressById } from "solecs/utils.sol";
+import { BRGameComponent, ID as BRGameComponentID } from "components/BRGameComponent.sol";
+import { BRGame, BRGameStatus } from "common/BRGame.sol";
+
+uint256 constant ID = uint256(keccak256("system.BREndGameSystem"));
+
+contract BREndGameSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function execute(bytes memory arguments) public returns (bytes memory) {
+    uint256 gameEntity = abi.decode(arguments, (uint256));
+    BRGameComponent brGameComponent = BRGameComponent(getAddressById(components, BRGameComponentID));
+    BRGame memory game = brGameComponent.getValue(gameEntity);
+    brGameComponent.set(
+      gameEntity,
+      BRGame({ startTime: game.startTime, rechargeTime: game.rechargeTime, status: BRGameStatus.OVER })
+    );
+  }
+
+  function executeTyped(uint256 game) public returns (bytes memory) {
+    return execute(abi.encode(game));
+  }
+}
--- contracts/src/systems/BRLeaveGameSystem.sol
+++ contracts/src/systems/BRLeaveGameSystem.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import { System, IWorld } from "solecs/System.sol";
-import { getSystemAddressById } from "solecs/utils.sol";
+import { getSystemAddressById, getAddressById } from "solecs/utils.sol";
 import { BRPieceControllerSystem } from "common/BRPieceControllerSystem.sol";
 import { ID as BRSetPieceSystemID } from "systems/BRSetPieceTypeSystem.sol";
 import { ID as BRMovePieceSystemID } from "systems/BRMovePieceSystem.sol";
+import { BRInGameComponent, ID as BRInGameComponentID } from "components/BRInGameComponent.sol";
+import { BRIsAliveComponent, ID as BRIsAliveComponentID } from "components/BRIsAliveComponent.sol";
 
-uint256 constant ID = uint256(keccak256("system.BRRevokeControllerSystem"));
+uint256 constant ID = uint256(keccak256("system.BRLeaveGameSystem"));
 
-contract BRRevokeControllerSystem is System {
+contract BRLeaveGameSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function execute(bytes memory arguments) public returns (bytes memory) {
     uint256 piece = abi.decode(arguments, (uint256));
+    // Cleanup components
+    BRInGameComponent brInGameComponent = BRInGameComponent(getAddressById(components, BRInGameComponentID));
+    BRIsAliveComponent brIsAliveComponent = BRIsAliveComponent(getAddressById(components, BRIsAliveComponentID));
+    brInGameComponent.remove(piece);
+    brIsAliveComponent.remove(piece);
+
+    // Revoke system access
     BRPieceControllerSystem brSetPieceSystem = BRPieceControllerSystem(
       getSystemAddressById(components, BRSetPieceSystemID)
     );
