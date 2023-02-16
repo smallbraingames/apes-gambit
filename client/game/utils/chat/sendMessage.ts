7617e2f0b350b4aa56c7a38fe1ec57e1218fec60
smallbraingames <smallbraindev@gmail.com>
2023-02-16 08:49:32 -0800

    feat(client): create chat message component and system
    - store the entire message as a string and be able to decode it

---
+++ client/game/utils/chat/sendMessage.ts
@@ -0,0 +1,23 @@
+const sendMessage = async (
+  message: string,
+  pieceEntity: string,
+  wallet: string
+): Promise<void> => {
+  const response = await fetch("/api/chat/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message, pieceEntity, wallet }),
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to send message: ${response.status} ${response.statusText}`
+    );
+  }
+};
+
+(window as any).sendMessage = sendMessage;
+
+export default sendMessage;
