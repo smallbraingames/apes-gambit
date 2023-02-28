15614531aeea9d881cb3a440894fb50cf4443acb
smallbraingames <smallbraindev@gmail.com>
2023-02-27 17:36:03 -0800

    feat(client): add tile variation

--- client/pages/dev.tsx
+++ client/pages/dev.tsx
@@ -6,9 +6,9 @@ import { NetworkContext } from "../context/NetworkContext";
 import { getEntityFromEntityIndex } from "../game/utils/resolveEntity";
 
 const GAME_START_TIME = Math.floor(new Date().getTime() / 1000) + 60 * 5;
-const GAME_RECHARGE_TIME = 7;
+const GAME_RECHARGE_TIME = 5;
 const GAME_INITIAL_GRID_DIM = 20;
-const GAME_SECONDS_PER_GRID_SHRINK = 15;
+const GAME_SECONDS_PER_GRID_SHRINK = 50000;
 const PERLIN_DENOM = 5;
 const PERLIN_THRESHOLD_BANANA = 700;
 const PERLIN_SEED = 0;
