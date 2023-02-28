4538a9002e5ed6c4a35c37f56c8889422d4c0be9
smallbraingames <smallbraindev@gmail.com>
2023-02-28 10:40:08 -0800

    feat(client): falling animation for obstacles

--- client/game/utils/setup/joinMainGame.ts
+++ client/game/utils/setup/joinMainGame.ts
@@ -1,6 +1,13 @@
-import { EntityID, EntityIndex, getComponentValue } from "@latticexyz/recs";
+import {
+  EntityID,
+  EntityIndex,
+  getComponentValue,
+  getComponentValueStrict,
+} from "@latticexyz/recs";
 
+import { GameStatus } from "../../types";
 import { Network } from "../../../network/types";
+import { getEntityIndexFromEntity } from "../resolveEntity";
 import joinGame from "../joinGame";
 import revokeGameControllersIfNecessary from "./revokeGameControllersIfNecessary";
 
@@ -10,15 +17,25 @@ const joinMainGame = async (
   activePiece: EntityIndex
 ) => {
   const {
-    components: { BRInGame },
+    world,
+    components: { BRInGame, BRGame },
   } = network;
-  await revokeGameControllersIfNecessary(network, activePiece);
   const playerGame = getComponentValue(BRInGame, activePiece)?.value;
   // @ts-ignore
   if (playerGame && playerGame === gameEntity) {
     console.log("Already in game");
     return;
   }
+  const game = getComponentValue(
+    BRGame,
+    // @ts-ignore
+    getEntityIndexFromEntity(gameEntity, world)
+  );
+  if (game && game.status !== GameStatus.NOT_STARTED) {
+    console.log("Not attempting to join game that has not started");
+    return;
+  }
+  await revokeGameControllersIfNecessary(network, activePiece);
   await joinGame(activePiece, gameEntity, network);
 };
 
