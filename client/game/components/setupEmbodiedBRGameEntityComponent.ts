426c5dd85b780ac8ace31d82c9bc65b644e35d32
smallbraingames <smallbraindev@gmail.com>
2023-02-27 22:43:16 -0800

    feat(client): query supabase for upcoming games

--- client/game/components/setupEmbodiedBRGameEntityComponent.ts
+++ client/game/components/setupEmbodiedBRGameEntityComponent.ts
@@ -1,23 +1,49 @@
-import { CurrentGameResponse } from "../../pages/api/currentGame";
+import {
+  EntityID,
+  EntityIndex,
+  getComponentValue,
+  getComponentValueStrict,
+  setComponent,
+} from "@latticexyz/recs";
+
+import { CurrentGameResponse } from "../../pages/api/nextGame";
 import { Game } from "../types";
 import { Network } from "../../network/types";
-import { setComponent } from "@latticexyz/recs";
 
 const setupEmbodiedBRGameEntityComponent = async (
   network: Network,
   game: Game
 ) => {
   const {
-    components: { EmbodiedBRGameEntity },
+    components: { EmbodiedBRGameEntity, ActivePiece },
   } = game;
-  const { godEntityIndex } = network;
+  const {
+    godEntityIndex,
+    components: { BRInGame },
+  } = network;
 
-  const response = await fetch("/api/currentGame");
+  const activePiece = getComponentValueStrict(ActivePiece, godEntityIndex)
+    .value as EntityIndex;
+
+  // @ts-ignore
+  const gameEntity = getComponentValue(BRInGame, activePiece)
+    ?.value as EntityID;
+
+  // If in game, set game we are in
+  if (gameEntity) {
+    setComponent(EmbodiedBRGameEntity, godEntityIndex, {
+      value: gameEntity as string,
+    });
+    return;
+  }
+
+  // If no game entity, get next game
+  const response = await fetch("/api/nextGame");
   const data: CurrentGameResponse = await response.json();
-
   setComponent(EmbodiedBRGameEntity, godEntityIndex, {
     value: data.gameEntity as string,
   });
+
   return [];
 };
 
---
+++ client/pages/api/nextGame.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { EntityID } from "@latticexyz/recs";
+import { createClient } from "@supabase/supabase-js";
+
+export type CurrentGameResponse = {
+  gameEntity: EntityID;
+};
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.SUPABASE_ADMIN_KEY as string
+);
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<CurrentGameResponse>
+) => {
+  const now = new Date();
+
+  const { data: games, error } = await supabase.from("games").select("*");
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  let game;
+  if (games && games.length > 0) {
+    let closestGame = null;
+    let closestDiff = Infinity;
+    for (let i = 0; i < games.length; i++) {
+      const startTime = new Date(games[i].start_time).getTime();
+      if (startTime > now.getTime()) {
+        const diff = startTime - now.getTime();
+        if (diff < closestDiff) {
+          closestGame = games[i];
+          closestDiff = diff;
+        }
+      }
+    }
+    game = closestGame;
+  } else {
+    console.error("No games found.");
+  }
+  const gameEntity = game.game_entity as EntityID;
+  res.status(200).json({ gameEntity });
+};
+
+export default handler;
