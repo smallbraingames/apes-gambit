700637994b37d749c9d88a50b3b8314905816670
smallbraingames <smallbraindev@gmail.com>
2023-03-04 16:37:43 -0800

    feat(client): camera center instructions

---
+++ client/components/br/Info.tsx
@@ -0,0 +1,12 @@
+const Info = () => {
+  return (
+    <div className="flex items-center gap-2 bg-yellow-50 bg-opacity-50 rounded-lg">
+      <div className="container h-10 w-10 flex items-center justify-center font-bold text-lg font-mono">
+        C
+      </div>
+      <p className="font-bold text-yellow-900">to center piece</p>
+    </div>
+  );
+};
+
+export default Info;
